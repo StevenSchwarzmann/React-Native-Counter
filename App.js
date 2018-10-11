7318567e36af2251080ced473e4713af636660f7
Steve Schwarzmann <steven.schwarzmann@gmail.com>
2018-10-10 21:09:11 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import EventList from './eventList';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <EventList />
+    );
+  }
+}
+
